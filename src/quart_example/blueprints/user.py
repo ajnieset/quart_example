227631c6729dc5c244d1448a93bbd14a031c8e9f
Austin Nieset <ajnieset@gmail.com>
2022-07-25 21:51:15 -0400

    renamed User model to User

--- src/quart_example/blueprints/user.py
+++ src/quart_example/blueprints/user.py
@@ -5,7 +5,7 @@ from orm import NoMatch
 from quart import Blueprint
 from quart_schema import validate_request, validate_response
 
-from quart_example.models.user import User as UserModel
+from quart_example.models.user import User
 
 blueprint = Blueprint("users", __name__)
 
@@ -30,7 +30,7 @@ class Users:
 @blueprint.get("/users/")
 # @validate_response(Users, 200)
 async def get_users():
-    users = await UserModel.objects.all()
+    users = await User.objects.all()
     users = [user.__dict__ for user in users]
     return Users(users=users), 200
 
@@ -39,7 +39,7 @@ async def get_users():
 @validate_response(UserData, 200)
 async def get_user(user_id):
     try:
-        user = await UserModel.objects.get(id=user_id)
+        user = await User.objects.get(id=user_id)
     except NoMatch:
         return {}, 404
     return UserData(**user.__dict__), 200
@@ -49,11 +49,11 @@ async def get_user(user_id):
 @validate_request(UserIn)
 @validate_response(UserData, 201)
 async def create_user(data: UserIn):
-    new_user: UserModel = await UserModel.objects.create(**asdict(data))
+    new_user: User = await User.objects.create(**asdict(data))
     return UserData(**new_user.__dict__), 201
 
 
 @blueprint.delete("/users/<int:user_id>")
 async def delete_user(user_id):
-    await UserModel.objects.filter(id=user_id).delete()
+    await User.objects.filter(id=user_id).delete()
     return {}, 204
